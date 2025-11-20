ebde213a234e3a82d1ae788f6327004639c442e8
gluonZeta <andrainat@gmail.com>
2025-11-20 04:24:09 +0100

    Cleaning for pur coverage sonar configuration

--- src/main/java/gluon/projects/exceptions/TechnicalProjectException.java
+++
@@ -1,7 +0,0 @@
-package gluon.projects.exceptions;
-
-public class TechnicalProjectException extends RuntimeException {
-    public TechnicalProjectException(Throwable cause) {
-        super(cause);
-    }
-}
--- src/main/java/gluon/projects/services/ListCryptoService.java
+++
@@ -1,5 +0,0 @@
-package gluon.projects.services;
-
-public interface ListCryptoService {
-
-}
