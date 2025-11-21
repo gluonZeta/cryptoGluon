7325f956eab54c8c5b1fbc9b5ac4e2c67993e024
gluonZeta <andrainat@gmail.com>
2025-11-21 07:23:44 +0100

    Add and initialisation of logback

--- src/main/java/gluon/projects/services/impl/TestCoverageServiceImpl.java
+++ src/main/java/gluon/projects/services/impl/TestCoverageServiceImpl.java
@@ -1,15 +1,22 @@
 package gluon.projects.services.impl;
 
 import gluon.projects.services.TestCoverageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestCoverageServiceImpl implements TestCoverageService {
+
+    private static Logger logger = LoggerFactory.getLogger(TestCoverageServiceImpl.class);
+
     @Override
     public int addFunction(int a, int b) {
+        logger.info("Call add ++ function");
         return a+b;
     }
 
     @Override
     public int multiplyFunction(int a, int b) {
+        logger.info("Call multiply XX function");
         return a*b;
     }
 }
