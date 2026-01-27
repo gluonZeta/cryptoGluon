651ac75585c51cb4922295c6529627585664249e
gluonZeta <andrainat@gmail.com>
2026-01-27 11:41:00 +0100

    Maintainability fix

--- src/main/java/gluon/projects/services/impl/SymbolCryptoServiceImpl.java
+++ src/main/java/gluon/projects/services/impl/SymbolCryptoServiceImpl.java
@@ -5,8 +5,6 @@ import gluon.projects.utilities.FileUtility;
 import gluon.projects.utilities.RestApiUtility;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
