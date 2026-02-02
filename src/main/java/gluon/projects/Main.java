6672a20cdc9562a6ac63064fb7dcdb355afcc7fe
gluonZeta <andrainat@gmail.com>
2026-02-02 04:31:44 +0100

    Test lecture d'un fichier

--- src/main/java/gluon/projects/Main.java
+++ src/main/java/gluon/projects/Main.java
@@ -1,5 +1,6 @@
 package gluon.projects;
 
+import gluon.projects.exceptions.TechnicalException;
 import gluon.projects.utilities.FileUtility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,28 +10,37 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
+import java.util.Random;
 
 public class Main {
 
     private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException {
         Properties properties = FileUtility.getPropertiesByFileName("application.properties");
         String filPath = properties.getProperty("filePath");
 
-        logger.info( "Test 0007" );
+
+
+        long nbLignes = Files.lines(Path.of(filPath)).count();
+        logger.info( "Test 0007: {}", nbLignes );
+        int randomIndex = new Random().nextInt((int) nbLignes);
 
 
         try (BufferedReader reader = Files.newBufferedReader(Path.of(filPath))) {
             String line;
+            int currentIndex = 0;
 
             while ((line = reader.readLine()) != null) {
-                if(line.contains("dog")) {
-                    logger.info(line);
+                if(currentIndex == randomIndex) {
+                    logger.info("LINE: {}", line);
+                    logger.info("LINE value: {}", currentIndex);
+                    break;
                 }
+                currentIndex++;
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new TechnicalException(e);
         }
     }
 }
